b926342e340f51f8b6e231ad97ccd4e866e8357d
Ameliorate <git-spam-or-if-human-remove-this-first-part-amelorate@amel.pw>
2021-01-26 15:48:05 -0500

    Add Comments
    Also fix nested lists not working, allow non-list values in file, and many other
    changes.

--- src/parse.rs
+++ src/parse.rs
@@ -1,22 +1,30 @@
 use nom::branch::alt;
 use nom::bytes::complete::tag;
-use nom::character::complete::{multispace0, multispace1, satisfy};
-use nom::combinator::{map, recognize};
-use nom::error::{VerboseError, convert_error, context};
+use nom::character::complete::{line_ending, multispace0, multispace1, not_line_ending, satisfy};
+use nom::combinator::{map, peek, recognize};
+use nom::error::{context, convert_error, VerboseError};
 use nom::multi::{many0, many1, separated_list0};
 use nom::number::complete::double;
 use nom::sequence::tuple;
 
 use super::*;
+use nom::lib::std::convert::identity;
 
 #[cfg(test)]
 mod test;
 
 pub fn file(input: &str) -> IResult<(), Vec<RispExp>, RispErr> {
-    let mut lists = context("file", many0(tuple((multispace0, list, multispace0))));
+    let mut lists = context("file", many0(tuple((multispace0, exp, multispace0))));
 
     match lists(input) {
-        Ok(("", items)) => Ok(((), items.iter().map(|(_, item, _)| item.clone()).collect())),
+        Ok(("", items)) => Ok((
+            (),
+            items
+                .iter()
+                .map(|(_, item, _)| item.clone())
+                .filter_map(identity)
+                .collect(),
+        )),
         Ok((rest, _items)) => Err(nom::Err::Error(RispErr::Reason(format!(
             "unknown text at end of file: {}",
             rest
@@ -33,46 +41,67 @@ pub fn file(input: &str) -> IResult<(), Vec<RispExp>, RispErr> {
     }
 }
 
-pub fn list(input: &str) -> IResult<&str, RispExp, VerboseError<&str>> {
-    let list_element = context("list_element", alt((bool, number, list, identifier)));
-    let mut list = context("list", tuple((
-        multispace0,
-        tag("("),
-        multispace0,
-        separated_list0(multispace1, list_element),
-        multispace0,
-        tag(")"),
-        multispace0,
-    )));
-
-    let (rest, (_, _, _, items, _, _, _)) = list(input)?;
-
-    Ok((rest, RispExp::List(items)))
+pub fn exp(input: &str) -> IResult<&str, Option<RispExp>, VerboseError<&str>> {
+    context("exp", alt((comment, bool, number, list, identifier)))(input)
 }
 
-pub fn bool(input: &str) -> IResult<&str, RispExp, VerboseError<&str>> {
+pub fn list(input: &str) -> IResult<&str, Option<RispExp>, VerboseError<&str>> {
+    let mut list = context(
+        "list",
+        tuple((
+            multispace0,
+            context("list_open_paren", tag("(")),
+            multispace0,
+            context("list_contents", separated_list0(multispace1, exp)),
+            multispace0,
+            context("list_close_paren", tag(")")),
+        )),
+    );
+
+    let (rest, (_, _, _, items, _, _)) = list(input)?;
+    let items = items.iter().filter_map(|opt| opt.clone()).collect();
+
+    Ok((rest, Some(RispExp::List(items))))
+}
+
+pub fn bool(input: &str) -> IResult<&str, Option<RispExp>, VerboseError<&str>> {
     let mut truefalse = context("boolean", alt((tag("true"), tag("false"))));
 
     let (rest, output) = truefalse(input)?;
 
     match output {
-        "true" => Ok((rest, RispExp::Bool(true))),
-        "false" => Ok((rest, RispExp::Bool(false))),
+        "true" => Ok((rest, Some(RispExp::Bool(true)))),
+        "false" => Ok((rest, Some(RispExp::Bool(false)))),
         _ => unreachable!(),
     }
 }
 
-pub fn number(input: &str) -> IResult<&str, RispExp, VerboseError<&str>> {
-    context("number", map(double, RispExp::Number))(input)
+pub fn number(input: &str) -> IResult<&str, Option<RispExp>, VerboseError<&str>> {
+    context("number", map(double, |n| Option::Some(RispExp::Number(n))))(input)
 }
 
-pub fn identifier(input: &str) -> IResult<&str, RispExp, VerboseError<&str>> {
-    let mut identifier = context("identifier", recognize(many1(satisfy(|char| {
-        !char.is_whitespace() && char != '(' && char != ')'
-    }))));
+pub fn identifier(input: &str) -> IResult<&str, Option<RispExp>, VerboseError<&str>> {
+    let mut identifier = context(
+        "identifier",
+        recognize(many1(satisfy(|char| {
+            !char.is_whitespace() && char != '(' && char != ')'
+        }))),
+    );
     // at least one character of anything but whitespace and parens
 
     let (rest, symbol) = identifier(input)?;
 
-    return Ok((rest, RispExp::Symbol(symbol.to_string())));
+    return Ok((rest, Some(RispExp::Symbol(symbol.to_string()))));
+}
+
+pub fn comment(input: &str) -> IResult<&str, Option<RispExp>, VerboseError<&str>> {
+    let mut comment = context(
+        "comment",
+        map(
+            tuple((tag(";"), not_line_ending, peek(line_ending))),
+            |_| Option::None,
+        ),
+    );
+
+    comment(input)
 }
--- src/parse/test.rs
+++ src/parse/test.rs
@@ -5,116 +5,128 @@ use super::*;
  */
 
 #[test]
-fn parse_bool_true() {
-    let (rest, result) = bool("true").unwrap();
+fn parse_bool_true() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = bool("true")?;
 
     assert_eq!(rest, "");
-    assert_eq!(result, RispExp::Bool(true));
+    assert_eq!(result, Some(RispExp::Bool(true)));
+    Ok(())
 }
 
 #[test]
-fn parse_bool_false() {
-    let (rest, result) = bool("false").unwrap();
+fn parse_bool_false() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = bool("false")?;
 
     assert_eq!(rest, "");
-    assert_eq!(result, RispExp::Bool(false));
+    assert_eq!(result, Some(RispExp::Bool(false)));
+    Ok(())
 }
 
 #[test]
-fn parse_number_float() {
-    let (rest, result) = number("12.1").unwrap();
+fn parse_number_float() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = number("12.1")?;
 
     assert_eq!(rest, "");
-    assert_eq!(result, RispExp::Number(12.1));
+    assert_eq!(result, Some(RispExp::Number(12.1)));
+    Ok(())
 }
 
 #[test]
-fn parse_number_int() {
-    let (rest, result) = number("12").unwrap();
+fn parse_number_int() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = number("12")?;
 
     assert_eq!(rest, "");
-    assert_eq!(result, RispExp::Number(12.0));
+    assert_eq!(result, Some(RispExp::Number(12.0)));
+    Ok(())
 }
 
 #[test]
-fn parse_identifier_alpha() {
-    let (rest, result) = identifier("is").unwrap();
+fn parse_identifier_alpha() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = identifier("is")?;
 
     assert_eq!(rest, "");
-    assert_eq!(result, RispExp::Symbol("is".to_string()));
+    assert_eq!(result, Some(RispExp::Symbol("is".to_string())));
+    Ok(())
 }
 
 #[test]
-fn parse_identifier_symbol() {
-    let (rest, result) = identifier("!").unwrap();
+fn parse_identifier_symbol() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = identifier("!")?;
 
     assert_eq!(rest, "");
-    assert_eq!(result, RispExp::Symbol("!".to_string()));
+    assert_eq!(result, Some(RispExp::Symbol("!".to_string())));
+    Ok(())
 }
 
 #[test]
-fn parse_identifier_spaces() {
-    let (rest, result) = identifier("test rest").unwrap();
+fn parse_identifier_spaces() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = identifier("test rest")?;
 
     assert_eq!(rest, " rest");
-    assert_eq!(result, RispExp::Symbol("test".to_string()));
+    assert_eq!(result, Some(RispExp::Symbol("test".to_string())));
+    Ok(())
 }
 
 #[test]
-fn parse_list_empty() {
-    let (rest, result) = list("()").unwrap();
+fn parse_list_empty() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = list("()")?;
 
     assert_eq!(rest, "");
-    assert_eq!(result, RispExp::List(Vec::new()));
+    assert_eq!(result, Some(RispExp::List(Vec::new())));
+    Ok(())
 }
 
 #[test]
-fn parse_list_numbers() {
-    let (rest, result) = list("(12 13.2 100)").unwrap();
+fn parse_list_numbers() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = list("(12 13.2 100)")?;
 
     assert_eq!(rest, "");
     assert_eq!(
         result,
-        RispExp::List(vec![
+        Some(RispExp::List(vec![
             RispExp::Number(12.0),
             RispExp::Number(13.2),
             RispExp::Number(100.0)
-        ])
+        ]))
     );
+    Ok(())
 }
 
 #[test]
-fn parse_list_empty_spaces() {
-    let (rest, result) = list("( )").unwrap();
+fn parse_list_empty_spaces() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = list("( )")?;
 
     assert_eq!(rest, "");
-    assert_eq!(result, RispExp::List(Vec::new()));
+    assert_eq!(result, Some(RispExp::List(Vec::new())));
+    Ok(())
 }
 
 #[test]
-fn parse_list_empty_newline() {
-    let (rest, result) = list("(\n)").unwrap();
+fn parse_list_empty_newline() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = list("(\n)")?;
 
     assert_eq!(rest, "");
-    assert_eq!(result, RispExp::List(Vec::new()));
+    assert_eq!(result, Some(RispExp::List(Vec::new())));
+    Ok(())
 }
 
 #[test]
-fn parse_list_spaces_after() {
-    let (rest, result) = list("() ").unwrap();
+fn parse_list_spaces_after() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = list("() ")?;
 
-    assert_eq!(rest, "");
-    assert_eq!(result, RispExp::List(Vec::new()));
+    assert_eq!(rest, " ");
+    assert_eq!(result, Some(RispExp::List(Vec::new())));
+    Ok(())
 }
 
 #[test]
-fn parse_list_nested() {
-    let (rest, result) = list("(test a (plus 1 2))").unwrap();
+fn parse_list_nested() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = list("(test a (plus 1 2))")?;
 
     assert_eq!(rest, "");
     assert_eq!(
         result,
-        RispExp::List(vec![
+        Some(RispExp::List(vec![
             RispExp::Symbol("test".to_string()),
             RispExp::Symbol("a".to_string()),
             RispExp::List(vec![
@@ -122,24 +134,29 @@ fn parse_list_nested() {
                 RispExp::Number(1.0),
                 RispExp::Number(2.0)
             ])
-        ])
+        ]))
     );
+    Ok(())
 }
 
 #[test]
-fn parse_list_multi_nested() {
-    let (rest, result) = list("(() ())").unwrap();
+fn parse_list_multi_nested() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = list("(() ())")?;
 
     assert_eq!(
         result,
-        RispExp::List(vec![RispExp::List(Vec::new()), RispExp::List(Vec::new())])
+        Some(RispExp::List(vec![
+            RispExp::List(Vec::new()),
+            RispExp::List(Vec::new())
+        ]))
     );
-    assert_eq!(rest, "")
+    assert_eq!(rest, "");
+    Ok(())
 }
 
 #[test]
-fn parse_file() {
-    let ((), result) = file("(test)\n(test2)").unwrap();
+fn parse_file() -> Result<(), nom::Err<RispErr>> {
+    let ((), result) = file("(test)\n(test2)")?;
 
     assert_eq!(
         result,
@@ -147,5 +164,27 @@ fn parse_file() {
             RispExp::List(vec![RispExp::Symbol("test".to_string())]),
             RispExp::List(vec![RispExp::Symbol("test2".to_string())])
         ]
-    )
+    );
+    Ok(())
+}
+
+#[test]
+fn parse_comment() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = comment("; This is a test\n")?;
+
+    assert_eq!(rest, "\n");
+    assert_eq!(result, None);
+    Ok(())
+}
+
+#[test]
+fn parse_comment_in_list() -> Result<(), nom::Err<VerboseError<&'static str>>> {
+    let (rest, result) = list("(test ; This is a test\n)")?;
+
+    assert_eq!(rest, "");
+    assert_eq!(
+        result,
+        Some(RispExp::List(vec![RispExp::Symbol("test".to_string())]))
+    );
+    Ok(())
 }
